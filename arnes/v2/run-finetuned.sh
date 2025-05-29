a7778c873f7b1ed6fd947aea57fecd7dd38b93af
Janez Kuhar <jk9292@student.uni-lj.si>
2025-05-29 08:04:09 +0200

    cleanup, add few shot

--- arnes/v2/run-finetuned.sh
+++ arnes/v2/run-finetuned.sh
@@ -11,17 +11,17 @@
 
 # Uncomment the following line to exclude a problematic node
 # from the list of potential nodes for your job
-#SBATCH --exclude=gwn04,gwn06             # Exclude problematic nodes from scheduling
+# #SBATCH --exclude=gwn02             # Exclude node gwn02 from scheduling
 
 # Display the hostname and GPU info
 echo "🧠 GPU info on node $HOSTNAME"
 nvidia-smi
 
-# CASE="examples_5_vrstic_sloberta90.json"
+CASE="examples_5_vrstic_sloberta90.json"
 # CASE="examples_5_vrstic_sloberta95.json"
 # CASE="examples_5_vrstic_sloberta99.json"
 # CASE="examples_10_vrstic_sloberta90.json"
-CASE="examples_10_vrstic_sloberta95.json"
+# CASE="examples_10_vrstic_sloberta95.json"
 # CASE="examples_10_vrstic_sloberta99.json"
 
 FEW_SHOT_COUNT=2
@@ -38,5 +38,6 @@ singularity exec --nv \
     python scripts/inference.py \
       --mode finetuned \
       --input_file /data/$CASE \
+      --few_shot_count $FEW_SHOT_COUNT \
       --output_file /outputs/{$FEW_SHOT_COUNT}_finetuned_results_$CASE
 "
